f1a51e27a8ff5a17b77be985e669e6c7327a8e6d
Alex <43142871+khyberspache@users.noreply.github.com>
2020-04-23 16:58:17 -0400

    adding argv for paw specification

--- agent/agent_factory.go
+++ agent/agent_factory.go
@@ -2,9 +2,9 @@ package agent
 
 // Creates and initializes a new Agent. Upon success, returns a pointer to the agent and nil Error.
 // Upon failure, returns nil and an error.
-func AgentFactory(server string, group string, c2Config map[string]string, enableP2pReceivers bool, initialDelay int) (*Agent, error) {
+func AgentFactory(server string, group string, c2Config map[string]string, enableP2pReceivers bool, initialDelay int, paw string) (*Agent, error) {
 	newAgent := &Agent{}
-	if err := newAgent.Initialize(server, group, c2Config, enableP2pReceivers, initialDelay); err != nil {
+	if err := newAgent.Initialize(server, group, c2Config, enableP2pReceivers, initialDelay, paw); err != nil {
 		return nil, err
 	} else {
 		newAgent.Sleep(newAgent.initialDelay)
--- sandcat.go
+++ sandcat.go
@@ -14,6 +14,7 @@ with command-line arguments at runtime.
 var (
 	key       = "JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA"
 	server    = "http://localhost:8888"
+	paw       = ""
 	group     = "red"
 	c2Name    = "HTTP"
 	c2Key     = ""
@@ -26,6 +27,7 @@ func main() {
 		parsedListenP2P = false
 	}
 	server := flag.String("server", server, "The FQDN of the server")
+	paw := flag.String("paw", paw, "Optionally specify a PAW on intialization")
 	group := flag.String("group", group, "Attach a group to this agent")
 	c2 := flag.String("c2", c2Name, "C2 Channel for agent")
 	delay := flag.Int("delay", 0, "Delay starting this agent by n-seconds")
@@ -35,5 +37,5 @@ func main() {
 	flag.Parse()
 
 	c2Config := map[string]string{"c2Name": *c2, "c2Key": c2Key}
-	core.Core(*server, *group, *delay, c2Config, *listenP2P, *verbose)
+	core.Core(*server, *group, *delay, c2Config, *listenP2P, *verbose, *paw)
 }
