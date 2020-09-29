607bf623809de436e57437bb4e593a0210df7489
Daniel Kim <kimd@mitre.org>
2020-09-29 14:36:36 -0400

    Removing unused variables and functions

--- privdetect/privilegedetect_windows.go
+++ privdetect/privilegedetect_windows.go
@@ -43,20 +43,6 @@ const (
 	errnoERROR_IO_PENDING = 997
 )
 
-var (
-	errERROR_IO_PENDING error = syscall.Errno(errnoERROR_IO_PENDING)
-)
-
-func errnoErr(e syscall.Errno) error {
-	switch e {
-	case 0:
-		return nil
-	case errnoERROR_IO_PENDING:
-		return errERROR_IO_PENDING
-	}
-	return e
-}
-
 func IsElevated(token syscall.Token) bool {
 	var isElevated uint32
 	var outLen uint32
