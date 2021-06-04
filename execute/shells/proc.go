e194a6fef3719843cd6916232d5c175f8d0ac409
Daniel Kim <kimd@mitre.org>
2021-06-04 07:51:01 -0400

    Adding process executor

---
+++ execute/shells/proc.go
@@ -0,0 +1,65 @@
+package shells
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"github.com/google/shlex"
+
+	"github.com/mitre/gocat/execute"
+	"github.com/mitre/gocat/output"
+)
+
+type Proc struct {
+	name string
+	currDir string
+}
+
+func init() {
+	cwd, _ := os.Getwd()
+    executor := &Proc{
+		name: "proc",
+		currDir: cwd,
+	}
+	execute.Executors[executor.name] = executor
+}
+
+func (p *Proc) Run(command string, timeout int, info execute.InstructionInfo) ([]byte, string, string) {
+	exePath, exeArgs, err := p.getExeAndArgs(command)
+	if err != nil {
+		output.VerbosePrint(fmt.Sprintf("[!] Error parsing command line: %s", err.Error()))
+		return nil, "", ""
+	}
+	output.VerbosePrint(fmt.Sprintf("[*] Starting process %s with args %v", exePath, exeArgs))
+	return runShellExecutor(*exec.Command(exePath, append(exeArgs)...), timeout)
+}
+
+func (p *Proc) String() string {
+	return p.name
+}
+
+func (p *Proc) CheckIfAvailable() bool {
+	return true
+}
+
+func (p *Proc) DownloadPayloadToMemory(payloadName string) bool {
+	return false
+}
+
+func (p *Proc) getExeAndArgs(commandLine string) (string, []string, error) {
+	output.VerbosePrint(fmt.Sprintf("[*] Processing commandline %s", commandLine))
+	if runtime.GOOS == "windows" {
+		commandLine = strings.ReplaceAll(commandLine, "\\", "\\\\")
+	}
+	split, err := shlex.Split(commandLine)
+	if err != nil {
+		return "", nil, err
+	}
+	execPath := split[0]
+	if strings.HasPrefix(execPath, ".") {
+		execPath = strings.Replace(execPath, ".", p.currDir, 1)
+	}
+	return execPath, split[1:], nil
+}
