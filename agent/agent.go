6d0445358910b13394ffb9eadbfd22c05204a7cf
Alexander Kent <akent@mitre.org>
2021-06-23 08:46:17 -0700

    recover panic

--- agent/agent.go
+++ agent/agent.go
@@ -1,6 +1,7 @@
 package agent
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,18 +10,17 @@ import (
 	"path/filepath"
 	"reflect"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
-	"context"
-	"strings"
 
+	"github.com/grandcat/zeroconf"
 	"github.com/mitre/gocat/contact"
 	"github.com/mitre/gocat/encoders"
 	"github.com/mitre/gocat/execute"
 	"github.com/mitre/gocat/output"
 	"github.com/mitre/gocat/privdetect"
 	"github.com/mitre/gocat/proxy"
-	"github.com/grandcat/zeroconf"
 )
 
 var beaconFailureThreshold = 3
@@ -49,40 +49,40 @@ type AgentInterface interface {
 // Implements AgentInterface
 type Agent struct {
 	// Profile fields
-	server string
-	tunnelConfig *contact.TunnelConfig
-	group string
-	host string
-	username string
-	architecture string
-	platform string
-	location string
-	pid int
-	ppid int
-	executors []string
-	privilege string
-	exe_name string
-	paw string
-	initialDelay float64
-	originLinkID int
-	hostIPAddrs []string
+	server                string
+	tunnelConfig          *contact.TunnelConfig
+	group                 string
+	host                  string
+	username              string
+	architecture          string
+	platform              string
+	location              string
+	pid                   int
+	ppid                  int
+	executors             []string
+	privilege             string
+	exe_name              string
+	paw                   string
+	initialDelay          float64
+	originLinkID          int
+	hostIPAddrs           []string
 	availableDataEncoders []string
 
 	// Communication methods
-	beaconContact contact.Contact
+	beaconContact       contact.Contact
 	failedBeaconCounter int
-	upstreamDestAddr string // address of server/peer that agent uses to contact C2
-	tunnel contact.Tunnel
-	usingTunnel bool
+	upstreamDestAddr    string // address of server/peer that agent uses to contact C2
+	tunnel              contact.Tunnel
+	usingTunnel         bool
 
 	// peer-to-peer info
-	enableLocalP2pReceivers bool
-	p2pReceiverWaitGroup *sync.WaitGroup
-	localP2pReceivers map[string]proxy.P2pReceiver // maps P2P protocol to receiver running on this machine
-	localP2pReceiverAddresses map[string][]string // maps P2P protocol to receiver addresses listening on this machine
-	availablePeerReceivers map[string][]string // maps P2P protocol to receiver addresses running on peer machines
-	exhaustedPeerReceivers map[string][]string // maps P2P protocol to receiver addresses that the agent has tried using.
-	usingPeerReceivers bool // True if connecting to C2 via proxy peer
+	enableLocalP2pReceivers   bool
+	p2pReceiverWaitGroup      *sync.WaitGroup
+	localP2pReceivers         map[string]proxy.P2pReceiver // maps P2P protocol to receiver running on this machine
+	localP2pReceiverAddresses map[string][]string          // maps P2P protocol to receiver addresses listening on this machine
+	availablePeerReceivers    map[string][]string          // maps P2P protocol to receiver addresses running on peer machines
+	exhaustedPeerReceivers    map[string][]string          // maps P2P protocol to receiver addresses that the agent has tried using.
+	usingPeerReceivers        bool                         // True if connecting to C2 via proxy peer
 
 	// Deadman instructions to run before termination. Will be list of instruction mappings.
 	deadmanInstructions []map[string]interface{}
@@ -120,7 +120,7 @@ func (a *Agent) Initialize(server string, tunnelConfig *contact.TunnelConfig, gr
 
 	a.hostIPAddrs, err = proxy.GetLocalIPv4Addresses()
 	if err != nil {
-	    return err
+		return err
 	}
 
 	// Paw will get initialized after successful beacon if it's not specified via command line
@@ -165,37 +165,37 @@ func (a *Agent) Initialize(server string, tunnelConfig *contact.TunnelConfig, gr
 // Returns full profile for agent.
 func (a *Agent) GetFullProfile() map[string]interface{} {
 	return map[string]interface{}{
-		"paw": a.paw,
-		"server": a.server,
-		"group": a.group,
-		"host": a.host,
-		"contact": a.GetCurrentContactName(),
-		"username": a.username,
-		"architecture": a.architecture,
-		"platform": a.platform,
-		"location": a.location,
-		"pid": a.pid,
-		"ppid": a.ppid,
-		"executors": a.executors,
-		"privilege": a.privilege,
-		"exe_name": a.exe_name,
-		"proxy_receivers": a.localP2pReceiverAddresses,
-		"origin_link_id": a.originLinkID,
-		"deadman_enabled": true,
+		"paw":                a.paw,
+		"server":             a.server,
+		"group":              a.group,
+		"host":               a.host,
+		"contact":            a.GetCurrentContactName(),
+		"username":           a.username,
+		"architecture":       a.architecture,
+		"platform":           a.platform,
+		"location":           a.location,
+		"pid":                a.pid,
+		"ppid":               a.ppid,
+		"executors":          a.executors,
+		"privilege":          a.privilege,
+		"exe_name":           a.exe_name,
+		"proxy_receivers":    a.localP2pReceiverAddresses,
+		"origin_link_id":     a.originLinkID,
+		"deadman_enabled":    true,
 		"available_contacts": contact.GetAvailableCommChannels(),
-		"host_ip_addrs": a.hostIPAddrs,
-		"upstream_dest": a.upstreamDestAddr,
+		"host_ip_addrs":      a.hostIPAddrs,
+		"upstream_dest":      a.upstreamDestAddr,
 	}
 }
 
 // Return minimal subset of agent profile.
 func (a *Agent) GetTrimmedProfile() map[string]interface{} {
 	return map[string]interface{}{
-		"paw": a.paw,
-		"server": a.server,
-		"platform": a.platform,
-		"host": a.host,
-		"contact": a.GetCurrentContactName(),
+		"paw":           a.paw,
+		"server":        a.server,
+		"platform":      a.platform,
+		"host":          a.host,
+		"contact":       a.GetCurrentContactName(),
 		"upstream_dest": a.upstreamDestAddr,
 	}
 }
@@ -264,9 +264,9 @@ func (a *Agent) RunInstruction(instruction map[string]interface{}, submitResults
 	result := make(map[string]interface{})
 	onDiskPayloads, inMemoryPayloads := a.DownloadPayloadsForInstruction(instruction)
 	info := execute.InstructionInfo{
-		Profile: a.GetTrimmedProfile(),
-		Instruction: instruction,
-		OnDiskPayloads: onDiskPayloads,
+		Profile:          a.GetTrimmedProfile(),
+		Instruction:      instruction,
+		OnDiskPayloads:   onDiskPayloads,
 		InMemoryPayloads: inMemoryPayloads,
 	}
 
@@ -287,8 +287,8 @@ func (a *Agent) RunInstruction(instruction map[string]interface{}, submitResults
 		a.beaconContact.SendExecutionResults(a.GetTrimmedProfile(), result)
 	}
 
- 	// Perform any uploads after sending execution results
- 	a.UploadFiles(instruction)
+	// Perform any uploads after sending execution results
+	a.UploadFiles(instruction)
 }
 
 func (a *Agent) UploadFiles(instruction map[string]interface{}) {
@@ -517,53 +517,60 @@ func (a *Agent) updateUpstreamComs(newComs contact.Contact) {
 	a.beaconContact = newComs
 }
 
-func (a *Agent) evaluateNewPeers(results <- chan *zeroconf.ServiceEntry) {
-    for entry := range results {
-        for _, ip := range entry.AddrIPv4 {
-            a.mergeNewPeers(entry.Text[0], fmt.Sprintf("%s:%d", ip, entry.Port))
-        }
-    }
+func (a *Agent) evaluateNewPeers(results <-chan *zeroconf.ServiceEntry) {
+	for entry := range results {
+		for _, ip := range entry.AddrIPv4 {
+			a.mergeNewPeers(entry.Text[0], fmt.Sprintf("%s:%d", ip, entry.Port))
+		}
+	}
 }
 
 func (a *Agent) mergeNewPeers(proxyChannel string, ipPort string) {
-    peer := fmt.Sprintf("%s://%s", strings.ToLower(proxyChannel), ipPort)
-    allPeers := append(a.availablePeerReceivers[proxyChannel], a.exhaustedPeerReceivers[proxyChannel]...)
-    for _, existingPeer := range allPeers {
-        if peer == existingPeer {
-            return
-        }
-    }
-    for protocol, addressList := range a.localP2pReceiverAddresses {
-        if proxyChannel == protocol {
-            for _, address := range addressList {
-                if peer == address {
-                    return
-                }
-            }
+	peer := fmt.Sprintf("%s://%s", strings.ToLower(proxyChannel), ipPort)
+	allPeers := append(a.availablePeerReceivers[proxyChannel], a.exhaustedPeerReceivers[proxyChannel]...)
+	for _, existingPeer := range allPeers {
+		if peer == existingPeer {
+			return
 		}
 	}
-    a.availablePeerReceivers[proxyChannel] = append(a.availablePeerReceivers[proxyChannel], peer)
-    output.VerbosePrint(fmt.Sprintf("[*] new peer added: %s", peer))
+	for protocol, addressList := range a.localP2pReceiverAddresses {
+		if proxyChannel == protocol {
+			for _, address := range addressList {
+				if peer == address {
+					return
+				}
+			}
+		}
+	}
+	a.availablePeerReceivers[proxyChannel] = append(a.availablePeerReceivers[proxyChannel], peer)
+	output.VerbosePrint(fmt.Sprintf("[*] new peer added: %s", peer))
 }
 
 func (a *Agent) DiscoverPeers() {
-    // Discover all services on the network (e.g. _workstation._tcp)
-    resolver, err := zeroconf.NewResolver(nil)
-    if err != nil {
-        output.VerbosePrint(fmt.Sprintf("[-] Failed to initialize zeroconf resolver: %s", err.Error()))
-    }
+	// Recover on any panic on the external module call and not take down the whole agent.
+	defer func() {
+		if err := recover(); err != nil {
+			output.VerbosePrint(fmt.Sprintf("[-] Panic occurred when calling zeroconf:", err))
+		}
+	}()
 
-    entries := make(chan *zeroconf.ServiceEntry)
-    go a.evaluateNewPeers(entries)
+	// Discover all services on the network (e.g. _workstation._tcp)
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		output.VerbosePrint(fmt.Sprintf("[-] Failed to initialize zeroconf resolver: %s", err.Error()))
+	}
 
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
-    defer cancel()
-    err = resolver.Browse(ctx, "_service._comms", "local.", entries)
-    if err != nil {
-         output.VerbosePrint(fmt.Sprintf("[-] Failed to browse for peers: %s", err.Error()))
-    }
+	entries := make(chan *zeroconf.ServiceEntry)
+	go a.evaluateNewPeers(entries)
 
-    <-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+	defer cancel()
+	err = resolver.Browse(ctx, "_service._comms", "local.", entries)
+	if err != nil {
+		output.VerbosePrint(fmt.Sprintf("[-] Failed to browse for peers: %s", err.Error()))
+	}
+
+	<-ctx.Done()
 }
 
 func (a *Agent) GetCurrentContactName() string {
