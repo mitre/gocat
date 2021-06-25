85814e4ae34027f5e1950c277e001c4f77554fb1
Daniel Kim <kimd@mitre.org>
2021-06-25 10:47:39 -0400

    Updating method signatures and agent link ID storage to match UUID format for link ID

--- agent/agent_factory.go
+++ agent/agent_factory.go
@@ -6,7 +6,7 @@ import (
 
 // Creates and initializes a new Agent. Upon success, returns a pointer to the agent and nil Error.
 // Upon failure, returns nil and an error.
-func AgentFactory(server string, tunnelConfig *contact.TunnelConfig, group string, c2Config map[string]string, enableLocalP2pReceivers bool, initialDelay int, paw string, originLinkID int) (*Agent, error) {
+func AgentFactory(server string, tunnelConfig *contact.TunnelConfig, group string, c2Config map[string]string, enableLocalP2pReceivers bool, initialDelay int, paw string, originLinkID string) (*Agent, error) {
 	newAgent := &Agent{}
 	if err := newAgent.Initialize(server, tunnelConfig, group, c2Config, enableLocalP2pReceivers, initialDelay, paw, originLinkID); err != nil {
 		return nil, err
